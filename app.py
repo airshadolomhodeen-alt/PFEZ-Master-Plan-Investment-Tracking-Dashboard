908bf9b52a9a161229c6a1b9605e80e200e92736
airshadolomhodeen-alt <airshadolomhodeen@gmail.com>
2026-09-19 12:19:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -66,15 +66,15 @@ st.sidebar.markdown("---")
 st.sidebar.markdown("### Project Lead")
 
 try:
-    st.sidebar.image("AirSad.png", width=130, caption="ENGR. AIRSAD R. OLOMODIN, MBA, CBE")
+    st.sidebar.image("AirSad.png", width=130)
 except Exception:
-    st.sidebar.image("https://img.icons8.com/fluency/96/user-male-circle.png", width=80, caption="Project Lead")
+    st.sidebar.image("https://img.icons8.com/fluency/96/user-male-circle.png", width=80)
 
 st.sidebar.markdown(
     """
-    <div style='font-size: 11px; color: #C9D1D9; line-height: 1.4;'>
-    <b>ENGR. AIRSAD R. OLOMODIN, MBA, CBE</b><br>
-    📱 0975-256-9055 / 0929-336-7787<br>
+    <div style='font-size: 13px; color: #FFFFFF; line-height: 1.5; font-weight: bold;'>
+    ENGR. AIRSAD R. OLOMODIN, MBA, CBE<br><br>
+    📱 0975-256-9055 / 0929-336-7787<br><br>
     ✉️ airsadolomodin@gmail.com
     </div>
     """,
